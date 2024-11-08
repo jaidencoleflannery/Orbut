b2a8c5761bfe4c71c9135db4d1af0a2a11d13734
Cole Flannery <jaidencoleflannery@gmail.com>
2024-11-08 15:41:15 -0600

    Fix merge issues.

--- Angular/src/app/dashboard/dashboard.component.ts
+++ Angular/src/app/dashboard/dashboard.component.ts
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 import { Component} from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { NotificationService } from '@services/notification.service';
-=======
-import { Component } from '@angular/core';
->>>>>>> f85d36c5cea518e8f3fb0829ebfbe6fe9868720a
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-<<<<<<< HEAD
-  imports: [JsonPipe],
-=======
   imports: [],
->>>>>>> f85d36c5cea518e8f3fb0829ebfbe6fe9868720a
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-<<<<<<< HEAD
   constructor(private NotificationService: NotificationService) {}
 
   notifications: any;
@@ -27,7 +18,3 @@ export class DashboardComponent {
     this.notifications = this.NotificationService.getNotifications;
   }
 }
-=======
-
-}
->>>>>>> f85d36c5cea518e8f3fb0829ebfbe6fe9868720a
